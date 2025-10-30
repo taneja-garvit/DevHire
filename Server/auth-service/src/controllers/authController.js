47a115b22b6a232787a6ed3371a384d211cd7ded
Garvit <taneja.garvit29@gmail.com>
2025-10-30 06:57:34 +0530

    updated controller

--- Server/auth-service/src/controllers/authController.js
+++ Server/auth-service/src/controllers/authController.js
@@ -51,3 +51,13 @@ export const login= async(req,res)=>{
         res.status(500).json({ message: err.message });
     }
 }
+
+export const getme = async(req,res)=>{
+     try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
