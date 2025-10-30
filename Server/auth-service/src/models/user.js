6f01a287c394ce32fd18a56e0807160fd5216e43
Garvit <taneja.garvit29@gmail.com>
2025-10-30 06:16:23 +0530

    updated schema

---
+++ Server/auth-service/src/models/user.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: {
+            type: String,
+            enum: ["developer","recruiter"],
+            required: true
+        }
+    },
+    { timestamps:true }
+
+);
+export default mongoose.model("User",userSchema);
