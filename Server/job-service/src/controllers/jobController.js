2e31d6a4c1e103f722f45277e3502d30d9e22651
Garvit <taneja.garvit29@gmail.com>
2025-10-30 19:47:38 +0530

    update jobcontroller

--- Server/job-service/src/controllers/jobController.js
+++ Server/job-service/src/controllers/jobController.js
@@ -10,3 +10,32 @@ export const createJob = async(req,res)=>{
     res.status(500).json({ message: "Server error", error: error.message });
     }
 };
+
+export const getAllJobs = async(req,res)=>{
+    try{
+        const filters={};
+        const {skill,location,experienceLevel } = req.query;
+
+        if(skill) filters.skill = {$regex: skill, $options: "i"}
+        if(location) filters.location = {$regex: location, $options: "i"}
+        if(experienceLevel) filters.location = experienceLevel;
+
+        const jobs = await jobs.find(filters).sort({ createdAt: -1 })
+        res.status(200).json({success:true, count:jobs.length,jobs})
+
+    } 
+    catch(err){
+        res.status(500)
+    }
+}
+
+export const getJobById = async(req,res)=>{
+    try{
+        const job = await Job.findById(req.params.id);
+        if(!job) return res.status(404).json({message:"Job not found"});
+        res.status(200).json({success:true,job})
+    }
+    catch(err){
+        return res.status(500).josn({message:"Server error",error: err.message})
+    }
+};
