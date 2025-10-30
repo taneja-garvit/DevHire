d84add8ee16d1fe223db5f3171d2f9490e563084
Garvit <taneja.garvit29@gmail.com>
2025-10-30 10:29:04 +0530

    middleware (verifyadmin)

---
+++ Server/job-service/src/middlewares/verifyAdmin.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+
+export const verifyAdmin = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader|| !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({ message: "No token provided" });
+
+    }
+    const token = authHeader.split(" ")[1];
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        if(decoded.role!==admin){
+            return res.status(403).json({ message: "Access denied: Admin only" });
+        }
+        req.user=decoded
+        next()
+    }
+    catch(err){
+        return res.status(401).json({message:"No token provided"})
+    }
+}
