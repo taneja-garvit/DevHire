2c49240c652a271a063886f6b0ff24da477e5be7
Garvit <taneja.garvit29@gmail.com>
2025-10-31 16:41:58 +0530

    updated db

--- Server/notification-service/src/db.js
+++ Server/notification-service/src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, { dbName: "scoreDB" });
-    console.log("Score DB Connected");
+    console.log("DB Connected");
   } catch (err) {
     console.error(err);
   }
