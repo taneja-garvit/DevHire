f171cb5dbbff9a1663e635bfa46ddec79dd26176
Garvit <taneja.garvit29@gmail.com>
2025-11-01 08:09:51 +0530

    update

---
+++ Server/score-service/src/grpc/server.js
@@ -0,0 +1,39 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import path from "path";
+import { fileURLToPath } from "url";
+import Score from "../models/Score.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PROTO_PATH = path.join(__dirname, "../../proto/devhire.proto");
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).devhire;
+
+async function SaveScore(call, callback) {
+  try {
+    const { userId, recruiterId, testId, score, total } = call.request;
+    const doc = await Score.create({ userId, recruiterId, testId, score, total });
+    callback(null, { resultId: doc._id.toString(), ok: true });
+  } catch (err) {
+    console.error("SaveScore error:", err);
+    callback({ code: grpc.status.INTERNAL, message: err.message });
+  }
+}
+
+export function startGrpcServer(port = "0.0.0.0:50052") {
+  const server = new grpc.Server();
+  server.addService(protoDescriptor.ScoreService.service, { SaveScore });
+  server.bindAsync(port, grpc.ServerCredentials.createInsecure(), (err, portUsed) => {
+    if (err) throw err;
+    server.start();
+    console.log(`Score gRPC server running at ${port}`);
+  });
+}
--- Server/score-service/src/server.js
+++ Server/score-service/src/server.js
@@ -2,13 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 import scoreRoutes from "./routes/scoreRoutes.js";
+import { startGrpcServer } from "./grpc/server.js";
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
+startGrpcServer("0.0.0.0:50051");
+
 app.use(express.json());
 app.use("/api/scores", scoreRoutes);
+
 
 const PORT = process.env.PORT || 4004;
 app.listen(PORT, () => console.log(`Score-Service running on port ${PORT}`));
