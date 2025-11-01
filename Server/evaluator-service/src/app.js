8369912d21db54116e4386a07e482ac36b84efbd
Garvit <taneja.garvit29@gmail.com>
2025-11-01 08:07:43 +0530

    updated grpc

--- Server/evaluator-service/src/app.js
+++ Server/evaluator-service/src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectdb from "./config/db.js";
 import evaluatorRoutes  from "./routes/evaluatorRoutes.js";
+import { startGrpcServer } from "./grpc/server.js";
+
 
 
 dotenv.config();
@@ -12,5 +14,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/evaluator",evaluatorRoutes);
+
+startGrpcServer("0.0.0.0:50051");
 
 export default app;
---
+++ Server/evaluator-service/src/grpc/server.js
@@ -0,0 +1,53 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = path.dirname(_filename)
+
+const PROTO_PATH = path.join(_dirname, "../../proto/devhire.proto")
+const packageDefinition = protoLoader.loadSync(PROTO_PATH,{
+      keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).devhire;
+
+async function EvaluateSubmission(call,callback) {
+    try {
+        const {testId, userId, recruiterId, answers} = call.request;
+
+        const result = await evaluateAndReturn({ testId, userId, recruiterId, answers });
+
+         callback(null, {
+      totalScore: result.totalScore,
+      totalQuestions: result.totalQuestions,
+      results: result.results.map(r => ({
+        questionId: r.questionId,
+        correct: r.correct,
+        pointsReceived: r.pointsReceived
+      }))
+    });
+
+    } catch (error) {
+         console.error("gRPC EvaluateSubmission error:", err);
+    callback({
+      code: grpc.status.INTERNAL,
+      message: err.message
+    });
+    }
+}
+
+export function startGrpcServer(port = "0.0.0.0:50051") {
+  const server = new grpc.Server();
+  server.addService(protoDescriptor.EvaluatorService.service, { EvaluateSubmission });
+  server.bindAsync(port, grpc.ServerCredentials.createInsecure(), (err, portUsed) => {
+    if (err) throw err;
+    server.start();
+    console.log(`Evaluator gRPC server running at ${port}`);
+  });
+}
